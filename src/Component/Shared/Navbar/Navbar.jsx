00d245e8f9cc88cc289d518694936e99f2f654bf
barkatzx <barkatullah.zx@gmail.com>
2023-06-08 01:05:38 +0600

    NavBar Added

--- src/Component/Shared/Navbar/Navbar.jsx
+++ src/Component/Shared/Navbar/Navbar.jsx
@@ -1,23 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AiFillHome } from 'react-icons/ai';
+import { CgClipboard } from 'react-icons/cg';
+import { FaUserCircle } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="bg-gray-800">
-      <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <span className="text-white text-lg font-semibold">Website Name</span>
-          </div>
-          <div className="flex items-center">
-            <a href="/" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
-            <a href="/instructors" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Instructors</a>
-            <a href="/classes" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Classes</a>
-            <a href="/dashboard" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</a>
-            <img src="user-profile-picture.jpg" alt="User Profile" className="h-8 w-8 rounded-full" />
-          </div>
+    <div className="bg-slate-100">
+      <nav className="flex items-center justify-between p-4 shadow-xl text-xl font-semibold">
+        <div className="flex items-center">
+          <Link to="/">
+          <img
+            src="https://i.ibb.co/cQ68nnZ/Arts-Institute-removebg-preview-1.png"
+            alt="Logo"
+            className="w-30 h-10 ml-4"
+          />
+          </Link>
         </div>
-      </div>
-    </nav>
+        <div className={isOpen ? 'flex flex-col md:flex-row md:relative md:top-16 md:right-0 md:bg-slate-100 w-full md:w-auto md:mt-0 md:flex md:items-center' : 'hidden md:flex md:items-center'}> 
+          <Link
+            to="/"
+            className="mr-4 hover:bg-indigo-800 rounded-3xl hover:text-gray-200 px-10 py-3 flex gap-2 items-center"
+          > <AiFillHome/>
+            Home
+          </Link>
+          <Link
+            to="/classes"
+            className="mr-4 hover:bg-indigo-800 rounded-3xl hover:text-gray-200 px-10 py-3 flex gap-2 items-center"
+          > <CgClipboard/>
+            Classes
+          </Link>
+          <Link
+            to="/instructors"
+            className="mr-4 hover:bg-indigo-800 rounded-3xl hover:text-gray-200 px-10 py-3 flex gap-2 items-center"
+          > <FaUserCircle/>
+            Instructors
+          </Link>
+        </div>
+        <div className="flex items-center">
+          <button className="bg-indigo-800 text-white py-3 px-10 rounded-3xl">
+            Login
+          </button>
+        </div>
+        <button
+            className="text-gray-700 hover:text-gray-900 focus:outline-none md:hidden"
+            onClick={handleToggle}
+          >
+            <svg
+              className="w-6 h-6 fill-current"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {isOpen ? (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M19 12H5a1 1 0 1 1 0-2h14a1 1 0 1 1 0 2zm0-7H5a1 1 0 1 1 0-2h14a1 1 0 1 1 0 2zm0 14H5a1 1 0 1 1 0-2h14a1 1 0 1 1 0 2z"
+                />
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M3 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 5.25zm0 7.5a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12zm0 7.25a.75.75 0 1 1 0-1.5h16.5a.75.75 0 0 1 0 1.5H3z"
+                />
+              )}
+            </svg>
+          </button>
+      </nav>
+    </div>
   );
 };
 
--- src/Layout/Home/Home.jsx
+++ src/Layout/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../Component/Shared/Navbar/Navbar';
+import Banner from '../../Component/Banner/Banner';
 
 const Home = () => {
     return (
         <div>
             <Navbar/>
+            <Banner/>
         </div>
     );
 };
