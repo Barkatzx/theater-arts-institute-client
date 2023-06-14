d9e113bf9aa34b80a84014d65a2d4341df4c46bc
barkatzx <barkatullah.zx@gmail.com>
2023-06-14 23:28:32 +0600

    Dashboard Update for all user

---
+++ src/Component/Dashboard/InstructorDashBoard/TotalEnrolled/TotalEnrolled.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TotalEnrolled = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TotalEnrolled;
---
+++ src/Component/Hooks/usePayment.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+import { useQuery } from "react-query";
+
+
+const usePayment = () => {
+  const { user, loading } = useContext(AuthContext);
+  const token = localStorage.getItem("access-token");
+  const { refetch, data: payment = [] } = useQuery({
+    queryKey: ["payment", user?.email],
+    enabled:
+      !loading && !!user?.email && !!localStorage.getItem("access-token"),
+    queryFn: async () => {
+      const res = await fetch(
+        `https://localhost:5000/payment?email=${user?.email}`,
+        {
+          headers: {
+            authorization: `bearer ${token}`,
+          },
+        }
+      );
+      return res.json();
+    },
+  });
+  return [payment, refetch];
+};
+
+export default usePayment;
