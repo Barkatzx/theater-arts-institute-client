eeb3023f410e2edaf3e54d4f04c65518d312b30d
barkatzx <barkatullah.zx@gmail.com>
2023-06-15 14:51:36 +0600

    Design Update

--- src/Component/PopularClasses/PopularClasses.jsx
+++ src/Component/PopularClasses/PopularClasses.jsx
@@ -1,33 +1,31 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import useAxiosSecure from "../Hooks/useAxiosSecure";
 import { useQuery } from "react-query";
 import ShowPopularClasses from "./ShowPopularClasses";
 import Title from "../Title/Title";
 
-
 const PopularClasses = () => {
   const [axiosSecure] = useAxiosSecure();
-  const [approvedClass, setApporvedClass] = useState([]);
-  const { data: classes = [], refetch } = useQuery(["classes"], async () => {
+  const [approvedClass, setApprovedClass] = useState([]);
+
+  const { data: classes = [] } = useQuery(["classes"], async () => {
     const res = await axiosSecure.get("/class");
     return res.data;
   });
+
   useEffect(() => {
-    const filter = classes.filter((classes) => classes.status === "approved");
-    setApporvedClass(filter);
+    const filter = classes.filter((classItem) => classItem.status === "approved");
+    setApprovedClass(filter);
   }, [classes]);
+
   console.log(approvedClass);
+
   return (
     <div className="mt-6 container mx-auto">
-      <Title
-        heading={"Popular Classes"}
-      ></Title>
-      <div className="grid md:grid-cols-3 gap-4 md:px-10 px-5 py-5">
-        {approvedClass.slice(0, 6).map((classes) => (
-          <ShowPopularClasses
-            key={classes._id}
-            classes={classes}
-          ></ShowPopularClasses>
+      <Title heading={"Popular Classes"} />
+      <div className="grid md:grid-cols-3 gap-4 px-5 py-5">
+        {approvedClass.slice(0, 6).map((classItem) => (
+          <ShowPopularClasses key={classItem._id} classes={classItem} />
         ))}
       </div>
     </div>
--- src/Component/Testimonial/Testimonial.jsx
+++ src/Component/Testimonial/Testimonial.jsx
@@ -1,18 +1,45 @@
 import React from "react";
+import Title from "../Title/Title";
 
 const Testimonial = () => {
   return (
-    <div className="max-w-md mx-auto bg-white rounded-lg overflow-hidden shadow-lg">
-      <div className="p-4">
+
+    <div className="mx-auto container mt-10">
+    <Title
+          heading={"Our Happy Clients"}
+        ></Title>
+        <div className="md:flex gap-6">
+        <div className="mx-auto bg-slate-100 rounded-2xl overflow-hidden shadow-2xl mt-5 p-4">
         <div className="w-20 h-20 bg-gray-200 rounded-full mx-auto"></div>
-        <h3 className="text-xl font-bold text-center mt-4">Client Name</h3>
-        <p className="text-gray-600 text-center">Company Name</p>
+        <h3 className="text-xl font-bold text-center mt-4">Jesica Lenin</h3>
+        <p className="text-gray-600 text-center">Arnolds</p>
         <p className="text-gray-800 mt-4">
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
           placerat posuere nibh, at commodo nulla consectetur a. Nullam dictum
           sapien et nisi efficitur sagittis."
         </p>
       </div>
+      <div className="p-4 mx-auto bg-slate-100 rounded-2xl overflow-hidden shadow-2xl mt-5">
+        <div className="w-20 h-20 bg-gray-200 rounded-full mx-auto"></div>
+        <h3 className="text-xl font-bold text-center mt-4">Jesica Lenin</h3>
+        <p className="text-gray-600 text-center">Arnolds</p>
+        <p className="text-gray-800 mt-4">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          placerat posuere nibh, at commodo nulla consectetur a. Nullam dictum
+          sapien et nisi efficitur sagittis."
+        </p>
+      </div>
+      <div className="p-4 mx-auto bg-slate-100 rounded-2xl overflow-hidden shadow-2xl mt-5">
+        <div className="w-20 h-20 bg-gray-200 rounded-full mx-auto"></div>
+        <h3 className="text-xl font-bold text-center mt-4">Jesica Lenin</h3>
+        <p className="text-gray-600 text-center">Arnolds</p>
+        <p className="text-gray-800 mt-4">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          placerat posuere nibh, at commodo nulla consectetur a. Nullam dictum
+          sapien et nisi efficitur sagittis."
+        </p>
+      </div>
+    </div>
     </div>
   );
 };
